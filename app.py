be821845c0b778114563342e6525ec38628756ca
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-05-14 11:42:48 +0000

    secondo commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-import pandas 
+import pandas as pd
+import os
+
 app = Flask(__name__)
 
 
@@ -13,16 +15,14 @@ def aggiungi():
         nuovo_profilo = {
             "Nome": request.form["nome"],
             "Cognome": request.form["cognome"],
-            "sport": request.form["sport"],
+            "Sport": request.form["sport"],
         }
-        df = pandas.read_csv("profile.csv")
-        df_nuovo = pandas.concat([df, pandas.DataFrame([nuovo_profilo])], ignore_index=True)
+        df = pd.read_csv("profile.csv")
+        df_nuovo = pd.concat([df, pd.DataFrame([nuovo_profilo])], ignore_index=True)
         df_nuovo.to_csv("profile.csv", index=False)
         return redirect(url_for('home'))
     
-    return render_template("aggiungi.html")
-
-
+    return render_template("aggiungi_prenotazione.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
