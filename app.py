6de1bbf380100a76156777069c78628c0382ecba
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-05-14 12:05:51 +0000

    terzo commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
-import os
+from datetime import datetime
+current_dateTime = datetime.now()
 
 app = Flask(__name__)
 
@@ -13,16 +14,32 @@ def home():
 def aggiungi():
     if request.method == "POST":
         nuovo_profilo = {
-            "Nome": request.form["nome"],
-            "Cognome": request.form["cognome"],
-            "Sport": request.form["sport"],
+            "nome": request.form["nome"],
+            "cognome": request.form["cognome"],
+            "sport": request.form["sport"],
+            "data e ora": datetime.now(),
+            "note opzionali": request.form["note opzionali"],
         }
+        
         df = pd.read_csv("profile.csv")
+        # Aggiungi il nuovo profilo
         df_nuovo = pd.concat([df, pd.DataFrame([nuovo_profilo])], ignore_index=True)
+        
+        # Salva il file CSV
         df_nuovo.to_csv("profile.csv", index=False)
+        
         return redirect(url_for('home'))
     
     return render_template("aggiungi_prenotazione.html")
+
+@app.route("/visualizza")
+def visualizza():
+    # Leggi il file CSV
+    df = pd.read_csv("profile.csv")
+    # Converti i dati in un dizionario per passarli al template
+    prenotazioni = df.to_dict(orient="records")
+    return render_template("visualizza_prenotazione.html", prenotazioni=prenotazioni)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
